ecde570d64c4102ab5489b8f78db7d0ec98b480e
lizaogang <email@Iven.com>
2024-12-04 14:39:00 +0800

    改变

--- src/main/java/com/Iven/test.java
+++ src/main/java/com/Iven/test.java
@@ -2,9 +2,6 @@ package com.Iven;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("输出");
-        System.out.println("第二");
-        System.out.println("第四");
-        System.out.println("第五");
+        System.out.println("master分支代码");
     }
 }
